ee9a9f33254f7b283ad5864264d878bfe8b5d364
Nick Gutierrez <ngutierrez@chicagotechacademy.org>
2025-02-07 16:28:13 -0600

    video 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    public Animator animator;
     private float horizontalMove = 0;
+    private bool isFacingRight = true;
     public float movementSpeed = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -21,5 +23,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxis("Horizontal") * movementSpeed;
+
+        if(horizontalMove != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else {
+            animator.SetBool("isRunning", false);
+        }
+
+
+        if(horizontalMove > 0 && !isFacingRight)
+        {
+            Flip();
+        }
+        else if(horizontalMove < 0 && isFacingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if(!isFacingRight)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        isFacingRight = !isFacingRight;
     }
 }
